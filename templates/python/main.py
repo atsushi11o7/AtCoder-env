0f13312f7960e282501d56ea284fe69d791e2414
atsushi11o7 <130956694+atsushi11o7@users.noreply.github.com>
2025-04-05 19:03:53 +0900

    script for the contest

--- templates/python/main.py
+++ templates/python/main.py
@@ -4,7 +4,11 @@ def main():
     # 入力
     S = input().strip()  # 文字列入力
     N = int(input().strip())  # 整数入力
+    N, M = map(int, input().split())  # 整数入力２
+    X = float(input().strip())  # 小数入力
     C = [input().strip() for _ in range(N)]  # N個の文字列の入力
+    A = list(map(int, input().split()))  # 整数列
+    N, Q = map(int, input().split())  # 複数の入力が一度にある場合
     
     # 問題解決のための処理
     result = solve(data)
